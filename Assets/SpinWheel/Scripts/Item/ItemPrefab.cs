3c131b1a7125e0e153d2ec50eb8f7a22e3c81c12
Ibrahim <ierkal@192.168.1.119>
2025-11-02 14:49:05 +0300

    ItemPrefab script added

---
+++ Assets/SpinWheel/Scripts/Item/ItemPrefab.cs
@@ -0,0 +1,33 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpinWheel.Scripts.Data.Item
+{
+    public class ItemPrefab : MonoBehaviour
+    {
+        [SerializeField] private Image _image;
+        [SerializeField] private TMP_Text _amountText;
+
+        private ItemData _itemData;
+
+        public void SetItemData(ItemData itemData)
+        {
+            _itemData = itemData;
+            if (_image) _image.sprite = itemData.IconSprite;
+            UpdateText();
+        }
+
+        public void UpdateText()
+        {
+            if (_amountText == null || _itemData == null) return;
+
+            // Show/hide for bombs if you want:
+            bool show = _itemData.ItemType != ItemType.Bomb;
+            _amountText.gameObject.SetActive(show);
+            if (!show) return;
+
+            _amountText.text = "X" + (int)_itemData.Amount;
+        }
+    }
+}
